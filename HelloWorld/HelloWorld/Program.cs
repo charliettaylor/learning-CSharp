365f5fe714396e01c88cd158e7610f7d6a798a1b
Peekoe <peekoecode@gmail.com>
2021-04-14 13:26:33 -0700

    mess around with I/O and conversions

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -10,9 +10,33 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            //using console
             Console.WriteLine("Hello World!");
             int var = Console.Read();
             Console.WriteLine("ASCII value is {0}:", var);
+            
+            //conversions
+            int myInt = 4;
+            double myDouble = 13.37;
+            //explicit conversion
+            myInt = (int)myDouble;
+            string myString = myDouble.ToString();
+            Console.WriteLine(myInt);
+            Console.WriteLine(myString);
+            //convert strings of ints into ints
+            myString = "14";
+            int convertString = Int32.Parse(myString);
+            Console.WriteLine(convertString);
+
+            //string methods
+            myString = "Hello World!";
+            string sub = myString.Substring(5);
+            Console.WriteLine("Substring: " + sub);
+            //use ToLower() or ToUpper() on it too
+            //Trim() works like strip() in Python, remove whitespace
+            int pos = myString.IndexOf("Hello");
+            Console.WriteLine("First hello is at " + pos);
+
             Console.ReadKey();
         }
     }
